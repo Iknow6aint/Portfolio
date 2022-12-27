c455f8c8f6a6820e94af04e78e02e5128c3e890b
iknow6aint <saintagbukor@gmail.com>
2022-12-27 17:19:35 -0400

    social links

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,40 +1,34 @@
 import React from "react";
 
 const About = () => {
-  return (
-    <div
-      name="about"
-      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
-    >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-            About
-          </p>
+    return (
+        <div
+            name="about"
+            className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+        >
+            <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+                <div className="pb-8">
+                    <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+                        About
+                    </p>
+                </div>
+
+                <p className="text-xl mt-20">
+                    My name is Saint Moses Agbukor I am from Edo state Nigeria.
+                    I am a software developer that has a strong passion for technology and a different kind of mindset from others.
+                    As a developer i am currently working as a freelancer  with over 2 years of software development.
+                </p>
+
+                <br />
+
+                <p className="text-xl">
+                    I am enthusiastic about life, a good learner and someone that can work alone
+                    and along side  other developers. I have other good communication skills and the ability
+                    to earn the respect  and build good relationships with people.
+                </p>
+            </div>
         </div>
-
-        <p className="text-xl mt-20">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
-          deserunt illum mollitia officiis qui exercitationem perferendis neque
-          quasi a recusandae necessitatibus tempora iusto! Blanditiis error
-          iste, totam fugiat recusandae rerum laborum perferendis molestiae
-          aperiam asperiores nemo. Magni dolor maxime debitis vitae, eaque hic
-          ab mollitia voluptatibus, a nostrum eveniet laborum!
-        </p>
-
-        <br />
-
-        <p className="text-xl">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
-          pariatur, vel similique sint, nobis aspernatur ut praesentium
-          explicabo ipsam aliquid quasi laboriosam et culpa possimus repudiandae
-          quisquam ullam maiores ab unde. Fugiat odio mollitia nemo alias.
-          Commodi facilis atque nulla vero voluptatem explicabo. Quibusdam,
-          magni quo! Eum cupiditate debitis labore.
-        </p>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default About;
